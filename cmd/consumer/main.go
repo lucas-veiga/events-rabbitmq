1dfaca46089ab677e294eed9e41ae01b646cc60c
Lucas Garcia <lucasveigagarcia@gmail.com>
2023-09-06 14:01:44 -0300

    add rabbitmq msg consumer

---
+++ cmd/consumer/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/lucas-veiga/events-rabbitmq/pkg/rabbitmq"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	ch, err := rabbitmq.OpenChannel()
+	if err != nil {
+		panic(err)
+	}
+	defer ch.Close()
+
+	msgs := make(chan amqp.Delivery)
+	go rabbitmq.Consume(ch, msgs)
+	for msg := range msgs {
+		println(string(msg.Body))
+		msg.Ack(false)
+	}
+}
